6b2e53c98dbb29c0cef8623a6e80aae025a04736
Roman_Barannyk <roman_barannyk@epam.com>
2023-01-19 13:51:27 +0200

    MODEMAIL-70 add logging to BaseRepository

--- src/main/java/org/folio/repository/BaseRepository.java
+++ src/main/java/org/folio/repository/BaseRepository.java
@@ -1,5 +1,7 @@
 package org.folio.repository;
 
+import static org.folio.util.LogUtil.asJson;
+
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -40,18 +42,24 @@ public class BaseRepository<T> {
   }
 
   public Future<List<T>> get(String query, int offset, int limit) {
+    log.debug("get:: parameters query: {}, offset: {}, limit: {}", query, offset, limit);
     CQLWrapper cql = new CQLWrapper(cql2pgJson, query, limit, offset);
     return pgClient.get(tableName, entityType, cql, true)
-      .map(Results::getResults);
+      .map(Results::getResults)
+      .onSuccess(result -> log.info("get:: result: {}", () -> asJson(result)));
   }
 
   public Future<List<T>> get(Criterion criterion) {
+    log.debug("get:: parameters criterion: {}", criterion);
     return pgClient.get(tableName, entityType, criterion, true)
-      .map(Results::getResults);
+      .map(Results::getResults)
+      .onSuccess(result -> log.info("get:: result: {}", () -> asJson(result)));
   }
 
   public Future<T> get(String id) {
-    return pgClient.getById(tableName, id, entityType);
+    log.debug("get:: parameters id: {}", id);
+    return pgClient.getById(tableName, id, entityType)
+      .onSuccess(result -> log.info("get:: result: {}", () -> asJson(result)));
   }
 
   public Future<List<T>> getAllWithDefaultLimit() {
@@ -59,36 +67,48 @@ public class BaseRepository<T> {
   }
 
   public Future<List<T>> getAllWithLimit(int limit) {
+    log.debug("getAllWithLimit:: parameters limit: {}", limit);
     return get(null, 0, limit);
   }
 
   public Future<String> save(T entity, String id) {
-    return pgClient.save(tableName, id, entity);
+    log.debug("save:: parameters entity: {}, id: {}", () -> asJson(entity), () -> id);
+    return pgClient.save(tableName, id, entity)
+      .onSuccess(result -> log.info("save:: result: {}", () -> asJson(result)));
   }
 
   public Future<String> upsert(T entity, String id) {
-    return pgClient.upsert(tableName, id, entity);
+    log.debug("upsert:: parameters entity: {}, id: {}", () -> asJson(entity), () -> id);
+    return pgClient.upsert(tableName, id, entity)
+      .onSuccess(result -> log.info("upsert:: result: {}", () -> asJson(result)));
   }
 
   public Future<Boolean> update(T entity, String id) {
+    log.debug("update:: parameters entity: {}, id: {}", () -> asJson(entity), () -> id);
     return pgClient.update(tableName, entity, id)
-      .map(updateResult -> updateResult.rowCount() == 1);
+      .map(updateResult -> updateResult.rowCount() == 1)
+      .onSuccess(result -> log.info("update:: result: {}", () -> asJson(result)));
   }
 
   public Future<Boolean> delete(String id) {
+    log.debug("delete:: parameters id: {}", id);
     return pgClient.delete(tableName, id)
-      .map(updateResult -> updateResult.rowCount() == 1);
+      .map(updateResult -> updateResult.rowCount() == 1)
+      .onSuccess(result -> log.info("delete:: result: {}", () -> asJson(result)));
   }
 
   public Future<Boolean> delete(Criterion criterion) {
+    log.debug("delete:: parameters criterion: {}", criterion);
     return pgClient.delete(tableName, criterion)
       .map(RowSet::rowCount)
       .onSuccess(rowCount -> log.info("Deleted {} record(s) from table {} using query: {}",
         rowCount, tableName, criterion))
-      .map(rowCount -> rowCount > 0);
+      .map(rowCount -> rowCount > 0)
+      .onSuccess(result -> log.info("delete:: result: {}", () -> asJson(result)));
   }
 
   public Future<Void> removeAll(String tenantId) {
+    log.debug("removeAll:: parameters tenantId: {}", tenantId);
     String deleteAllQuery = String.format("DELETE FROM %s_%s.%s", tenantId,
       ModuleName.getModuleName(), tableName);
     return pgClient.execute(deleteAllQuery).mapEmpty();
