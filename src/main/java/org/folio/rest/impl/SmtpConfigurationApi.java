39a727720797adb54170b37d6aafbdd54a490ae0
Roman_Barannyk <roman_barannyk@epam.com>
2023-01-19 16:01:49 +0200

    MODEMAIL-70 add logging to SmtpConfigurationApi

--- src/main/java/org/folio/rest/impl/SmtpConfigurationApi.java
+++ src/main/java/org/folio/rest/impl/SmtpConfigurationApi.java
@@ -1,9 +1,13 @@
 package org.folio.rest.impl;
 
+import static org.folio.util.LogUtil.headersAsString;
+
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.rest.jaxrs.model.SmtpConfiguration;
 import org.folio.rest.jaxrs.model.SmtpConfigurations;
 import org.folio.rest.persist.PgUtil;
@@ -14,11 +18,15 @@ import io.vertx.core.Handler;
 
 public class SmtpConfigurationApi implements org.folio.rest.jaxrs.resource.SmtpConfiguration {
   public static final String SMTP_CONFIGURATION_TABLE_NAME = "smtp_configuration";
+  private static final Logger logger = LogManager.getLogger(SmtpConfigurationApi.class);
 
   @Override
   public void getSmtpConfiguration(int offset, int limit, String query, String lang,
     Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext) {
+
+    logger.debug("getSmtpConfiguration:: parameters offset: {}, limit: {}, query: {}, lang: {}, " +
+        "okapiHeaders: {}", () -> offset, () -> limit, () -> query, () -> lang, () -> headersAsString(okapiHeaders));
 
     PgUtil.get(SMTP_CONFIGURATION_TABLE_NAME, SmtpConfiguration.class, SmtpConfigurations.class,
       query, offset, limit, okapiHeaders, vertxContext, GetSmtpConfigurationResponse.class)
@@ -30,6 +38,9 @@ public class SmtpConfigurationApi implements org.folio.rest.jaxrs.resource.SmtpC
     Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext) {
 
+    logger.debug("postSmtpConfiguration:: parameters lang: {}, okapiHeaders: {}",
+      () -> lang, () -> headersAsString(okapiHeaders));
+
     PgUtil.post(SMTP_CONFIGURATION_TABLE_NAME, entity, okapiHeaders, vertxContext,
       PostSmtpConfigurationResponse.class)
       .onComplete(asyncResultHandler);
@@ -39,6 +50,9 @@ public class SmtpConfigurationApi implements org.folio.rest.jaxrs.resource.SmtpC
   public void getSmtpConfigurationBySmtpConfigurationId(String smtpConfigurationId, String lang,
     Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext) {
+
+    logger.debug("getSmtpConfigurationBySmtpConfigurationId:: parameters smtpConfigurationId: {}, " +
+        "lang: {}, okapiHeaders: {}", () -> smtpConfigurationId, () -> lang, () -> headersAsString(okapiHeaders));
 
     PgUtil.getById(SMTP_CONFIGURATION_TABLE_NAME, SmtpConfiguration.class,
       smtpConfigurationId, okapiHeaders, vertxContext,
@@ -50,6 +64,9 @@ public class SmtpConfigurationApi implements org.folio.rest.jaxrs.resource.SmtpC
     SmtpConfiguration entity, Map<String, String> okapiHeaders,
     Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
 
+    logger.debug("putSmtpConfigurationBySmtpConfigurationId:: parameters smtpConfigurationId: {}, " +
+      "lang: {}, okapiHeaders: {}", () -> smtpConfigurationId, () -> lang, () -> headersAsString(okapiHeaders));
+
     PgUtil.put(SMTP_CONFIGURATION_TABLE_NAME, entity, smtpConfigurationId, okapiHeaders,
       vertxContext, PutSmtpConfigurationBySmtpConfigurationIdResponse.class)
       .onComplete(asyncResultHandler);
@@ -59,6 +76,9 @@ public class SmtpConfigurationApi implements org.folio.rest.jaxrs.resource.SmtpC
   public void deleteSmtpConfigurationBySmtpConfigurationId(String smtpConfigurationId, String lang,
     Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext) {
+
+    logger.debug("deleteSmtpConfigurationBySmtpConfigurationId:: parameters smtpConfigurationId: {}, " +
+      "lang: {}, okapiHeaders: {}", () -> smtpConfigurationId, () -> lang, () -> headersAsString(okapiHeaders));
 
     PgUtil.deleteById(SMTP_CONFIGURATION_TABLE_NAME, smtpConfigurationId, okapiHeaders,
       vertxContext, DeleteSmtpConfigurationBySmtpConfigurationIdResponse.class)
